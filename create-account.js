e9a5a6c4a564654e805ba0a12ae13414145d4def
Kristen Krofchik <k.krofchik@gmail.com>
2021-07-09 20:42:49 -0500

    ready to test, create-account.js

--- create-account.js
+++ create-account.js
@@ -1,5 +1,43 @@
 function createAccount(pin, amount) {
+    let balance = amount;
 
+    return {
+        
+        checkBalance(enteredPin) {
+            if(enteredPin === pin) {
+                return balance;
+            } else {
+                return 'Invalid PIN.'
+            }
+        },
+
+        deposit(enteredPin, depAmount) {
+            let bal = checkBalance(enteredPin);
+            if(bal === balance) {
+                balance += depAmount;
+            } else {
+                return 'Invalid PIN.'
+            }
+        },
+
+        withdraw(enteredPin, withdrawalAmount) {
+            let bal = checkBalance(enteredPin);
+            if(bal === balance) {
+                balance -= withdrawalAmount;
+            } else {
+                return 'Invalid PIN.'
+            }
+        },
+
+        changePin(enteredPin, newPin) {
+            if(enteredPin !== pin) {
+                return 'Invalid PIN.'
+            } else {
+                pin = newPin;
+            }
+        }
+
+    }
 }
 
 module.exports = { createAccount };
